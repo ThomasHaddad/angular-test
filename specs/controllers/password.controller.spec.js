7927d2178045919b4ca7cfe28248258e12418ad6
Thomas Haddad <thomas.af.haddad@gmail.com>
2015-03-24 14:48:33 +0100

    added .travis.yml and updated test karma.conf.js

--- specs/controllers/password.controller.spec.js
+++ specs/controllers/password.controller.spec.js
@@ -1,14 +1,14 @@
 describe('In order to check the strength of my password as a visitor i need to see the message strength of the password', function () {
-    var passwordControllerX;
-    var $scopeX;
+    var passwordController;
+    var $scope;
 
     beforeEach(function(){
        angular.mock.module('angular-test');
 
         angular.mock.inject(function($controller,$rootScope){
            $scope=$rootScope.$new();
-            passwordControllerX = $controller('passwordController',{
-                $scopeX:$scope
+            passwordController = $controller('passwordController',{
+                $scope:$scope
             });
         });
     });
@@ -16,19 +16,19 @@ describe('In order to check the strength of my password as a visitor i need to s
     describe("test grade method",function(){
         it('sets the strength to "weak" if the password is 12345', function() {
             // given
-            $scopeX.password = "12345";
+            $scope.password = "12345";
             // and
-            $scopeX.grade();
+            $scope.grade();
             // then
-            expect($scopeX.strength).toEqual("weak");
+            expect($scope.strength).toEqual("weak");
         });
         it('sets the strength to "strong" if the password is 12345678900', function() {
             // given
-            $scopeX.password = "12345678900";
+            $scope.password = "12345678900";
             // and
-            $scopeX.grade();
+            $scope.grade();
             // then
-            expect($scopeX.strength).toEqual("strong");
+            expect($scope.strength).toEqual("strong");
         });
     });
 });
